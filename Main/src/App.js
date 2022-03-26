ba43a23dff2a12c549b7070bb29676b21c5941f0
Narcisah <91091964+mel-ificent@users.noreply.github.com>
2022-03-26 12:21:49 -0400

    created initiatial public and src files

---
+++ Main/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './components/';
+import Navigation from './components/';
+import Project from './components/';
+import Footer from './components/';
+
+function App() {
+  return (
+    <div className="profile-app">
+      <Header />
+      <Navigation />
+      <Project />
+      <Footer />
+    </div>
+
+  );
+}
+
+export default App;
